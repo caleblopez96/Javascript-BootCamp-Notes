469ad1260ce4704f1b6a797100c35541b545ddf8
Caleb <87406555+caleblopez96@users.noreply.github.com>
2022-05-30 04:27:50 -0500

    Add files via upload

--- Bootcamp.js
+++ Bootcamp.js
@@ -212,65 +212,478 @@ let numOfDouts = 12; // 12 (number type)
 // just because you can doesnt mean you should. 
 // above is just an example of how you can (in console)
 
+//-----------------------------------------------------------------------------
+
+//Strings
+
+//Strings are pieces of text, or strings of characters
+//strings are always wrapped in quotes i.e: 'imAString'
+//Single or double quotes work, but be consistent
+//if you start with a single quote and dont end with a single quote the 
+//string technically doesnt end, so open the string and close the string with
+//the same set of quotation marks
+
+let stringExample1 = 'i am a string :)';
+console.log(stringExample1); // i am a string :)
+
+let msg = "Please do not feed the 'animals'!";
+console.log(msg); // Please do not feed the 'animals'!
+
+//string concatenation
+//adding two strings together to make one string
+
+let stringFirstName = 'Caleb'
+let stringLastName = ' Lopez'
+console.log(stringFirstName + stringLastName); // 'Caleb Lopez'
+
+let stringConcat = "I love";
+let stringConcat2 = "Javascript"
+console.log(stringConcat + " " + stringConcat2); // "I love Javascript" 
 
 
 
 
+// String Indices
+// strings are indexed by characters
+// each character is assigned its own numbers based off position in string
+//left to right starting at 0
+
+//example:
+//                  C H I C K E N
+//                  0 1 2 3 4 5 6
+
+//Strings come with properties 
+
+//.length property gives you the length in characters of the string
+
+let hello = 'hello'
+console.log(hello.length); // 5 ('hello' has 5 characters)
+//note every character including spacing and periods etc are a character
+
+let mySong = "Surfin' USA";
+console.log(mySong.length); // 11
+
+//how to access individual characters by their indices
+console.log(mySong[0]); // S
+console.log(mySong[3]); // f
+console.log(mySong[12]); //undefined (there is no character at index 12
+//so technically its undefined)
+
+//since strings are indexed and the .length is not, if you need to find the 
+//last character of a long string you can do it like this
+let gibberish = 'alkdjflkasjdfij;ls0  dkdjso 4';
+console.log(gibberish[gibberish.length - 1]); // 4
+
+//----------------------------------------------------------------------------
+
+//String Methods 
+//Strings come with a set of built-in methods, which are actions that can be
+//performed on or with that particular string
+//we can do things like 
+// searching within a string
+// replacing parts of a string
+// changing case (upper/lowercase)
+
+//syntax for string methods
+//                          string.method();
+
+//toUpperCase() / toLowerCase()
+
+let lowerToUpper = 'lower cased to upper case'
+console.log(lowerToUpper.toUpperCase()); // LOWER CASE TO UPPER CASE
+
+let upperToLower = 'UPPER CASE TO LOWER CASE';
+console.log(upperToLower.toLowerCase()); // upper case to lower case
+
+let newMsg = 'welcome to my website';
+console.log(newMsg.toUpperCase()); // WELCOME TO MY WEBSITE
+
+// below im getting my variable newMsg after its been upper cased, 
+// and assigning it to a new variable called yell. then calling yell which
+// gives me the value of newMsg uppercased printed to the console.
+
+let yell = newMsg.toUpperCase(); 
+console.log(yell) // WELCOME TO MY WEBSITE
+
+// DONT FORGET THE PARENTHESIS AFTER .toUpperCase/.toLowerCase
 
 
+// .trim()
+// trims the extra white space preceeding and following the string
+// trim will NOT remove white space between words, no matter how much 
+// space is present. it only removes the white space before and 
+// after the string, not anything in the middle
+
+let trimMe = "      get rid of the white space please      ";
+console.log(trimMe.trim()); //get rid of the white space please//
+// (with no white space)
 
 
+//using two methods on one string executes the first method listed, then 
+//proceeds to other methods if there are any
+let trimToUpper = trimMe.trim().toUpperCase();
+console.log(trimToUpper); //GET RID OF THE WHITE SPACE PLEASE//
 
 
+// some methods require arguments to be passed to it. arguments are essentially
+// extra values needed to complete the method
+// these arguments are passed into the method via the parenthesis
+
+// syntax
+//                      thing.method(arguments)
+
+//.indexOf
+// basically gives you the index of something
+// in the examples below we're finding the index of a string within in a string
+// aka a substring
+
+let tvShow = 'catdog';
+console.log(tvShow.indexOf('cat')); // 0 (cat starts at index 0)
+console.log(tvShow.indexOf('dog')); // 3 (dog starts at index 3)
+console.log(tvShow.indexOf('z')); // -1 (the 'not found' value)
+
+let sport = 'baseball';
+console.log(sport.indexOf('ball')); // 4 (ball starts at indec 4)
+
+// if indexOf something is = -1 then you know its not there. this is used 
+// when writing logic to see if the thing youre looking for is there
 
 
+//.slice()
+// slice takes 'slices' of an exisiting string and gives you pieces 
+//                       thing.slice(start, end)
+//first value passed to slice is where it starts and the last value is where it
+// ends. note if no ending value is given
+
+let longStr = 'supercalifragilisticexpialidocious';
+console.log(longStr.slice(0,5)); // 'super'
+// the console logs 'super' because of the values 0, and 5 that were passed
+// to the method. 
+// it starts at index 0 and ends at index 5 but stops at 5 and returns
+// everything before it
+console.log(longStr.slice(5)); // califragilisticexpialidocious
+
+let superhero = 'spiderman'
+console.log(superhero.slice(0,6)); // spider
+console.log(superhero.slice(6)); // man
+console.log(superhero.slice(7,9)); // an
+console.log(superhero.slice(1,3)); // pi
 
 
+console.log(sport.slice(4)); // ball(starts at index 4 & goes to end of string)
+
+console.log(sport.slice(40)); // '' theres nothing at index 40 so it returns 
+//nothing;
+
+let shortStr = (sport.slice(5));
+let sliced = shortStr;
+console.log(sliced); // all
+// the variable sliced is now equal to 'all'
+
+let savings = '$50.60';
+console.log(savings.slice(1)); // 50.60 (slices the dollar sign)
+//this is a common use case when you have data that follows a pattern and
+//youre trying to isolate a certain protion of it
 
 
+// .replace()
+// replaces a value with another value 
+
+//if the values passed in are not found, it just wont replace anything.
 
 
+let annoyingLaugh = 'teehee so funny! teehee!';
+console.log(annoyingLaugh.replace('teehee', 'haha')); // haha so funny! teehee!
+//replaces teehee with haha
+//notice it only replaces the first instance of teehee
+// replace(this, with this)
+console.log(sport.replace('ball','bull')); // basebull
+console.log(sport);
+// This does not change the value of the variable
+
+//                           string quiz
+
+// const age = "5" + "4"
+//console.log(age) // "54" (it concatenates strings)
+
+//let pecanPie = "pecan pie";
+//console.log(pecanPie [7]); // i (i is in index position 7)
+
+//let pup = 'pup';
+//console.log(pup[3]); // undefined (there is no letter at index 3 in variable 
+//pup)
+
+//let song = 'london calling';
+//console.log(song.toUpperCase()); // LONDON CALLING (.toUpperCase() does not 
+//require a value to be given inside the parenthesis)
+
+//let userEmail = '   CALEBLOPEZ96@gmail.com          ';
+//console.log(userEmail.trim().toLowerCase()); // caleblopez96@gmail.com (trims //the white space and the lowercases everything)
+
+//let park = 'yellowstone';
+//const parkIndex = park.indexOf('stone');
+//console.log(parkIndex); // 6
+
+let yelling = 'GO AWAY!!';
+let yellingIndex = yelling.indexOf('!');
+console.log(yellingIndex); // 7
+
+let trash = 'GARBAGE';
+console.log(trash.toLowerCase().slice(2).replace('b', '')); // rage
+// the case sensitivity of 'b' matters. due to using the method .toLowerCase,
+//i had to make the replace method look for a lower case b even tho the 
+// original variable of GARBAGE had a capital B.
 
 
+// String Escapes (aka escape characters, escape notation, escape sequence etc)
+// they allow you to use other wise illegal or functional (/,-) characters
+// so javascript wont recognize them
 
-//string template literal practice
-//string template literals always start with a backtick and the 
-//template literal goes in between ${}
+// 'he said 'lalal'' // this is invalid because we used single quotes twice 
+// in a string javascript intrerprets it as 'he said 'lalal ''
+//                               he said with a space lalal then empty quotes
+// the simple solution is to do this "he said 'lalala'"// this is valid
+
+// \n - is a new line
+// \' - single quote
+// \" - double quote
+// \\ - backslash
+
+
+//single quote escape
+let saying = 'he said he ain\'t happy'
+console.log(saying); // he said he ain't happy
+// the \' allowed me to use a single quote inside a single quote because
+// it told javascript to ignore it
+
+
+//double quote escape
+let sayingTwo = "he said \"he aint happy\""
+console.log(sayingTwo); // he said "he aint happy"
+
+//new line escape
+let sayingThree = 'he said\nthis is a new line';
+console.log(sayingThree); // he said
+//                           this is a new line
+
+//backslash example
+let sayingFour = 'he said this \\ is a backslash';
+console.log(sayingFour);
+
+
+//-----------------------------------------------------------------------------
+
+//string template literals `${}`
+//template literals are strings that allow embedded expressions, which will be //evaluated and then turned into a resulting string
+//special strings that take in information expressions, other variables, data
+//etc, and spit out one variable at the end
+//data is being interpolated inside the string
+// syntax:
+//                              `${}` 
+//(back ticks, dollar sign, open/close curly braces)
+// `i am a template literal` (cuz the backticks) 
+
+// not supported in internet explorer
+
+let numSheep = 3 + 4;
+console.log(`I counted ${numSheep} sheep`); // I counted 7 sheep 
+
 let username = 'dableb';
 console.log(`Welcome back, ${username}!`);
+
+let endGameMsg = `GAME OVER ${username.toUpperCase()}.\nTRY AGAIN?`;
+console.log(endGameMsg); // GAME OVER DABLEB.
+//                          TRY AGAIN?
+
+let item = 'cucumbers';
+let price = 1.50;
+let quantity = 4;
+console.log(`You bought ${quantity} ${item}, total price: $${price * quantity}`);
+// price * quantity has 2 dollar signs because once the data is interpolated
+// and returned as a string, I still want a dollar sign there to represent
+// that the price is in dollars
+
+let elapsedTime = 3;
+// let username = 'dableb' (commented out because already declared this var)
+console.log(`posted ${elapsedTime} minutes ago by ${username}`); 
+// posted 3 minutes ago by dableb
 
 const minAge = 21;
 let yourAge = 19;
 console.log(`you must be ${minAge} to enter. Please come back in ${minAge - yourAge} years`)
 
 
-//some Math.method() examples 
+// Null and Undefined
+// both represent the idea of nothingness, or the lack of a value, but 
+// theyre different
+// one of the main differences is we dont normally set values to undefined. // you can, but we dont usually do it. normally undefined is a line we get 
+// back from Javascript telling us a that the var refrenced is undefined
 
-//Math.random() always gives you a decimal between 0.000 repeating
-//and 1 so its necessary to multiply it by 10 to get a whole number
+//null - "intentional absence of any value"
+//  null must be assigned
+let nothing = null;
+console.log(nothing); // null
+
+// pretend no one is logged in
+let loggedInUser = null;
+// a user logs in, the variable is given a value & it no longer is undefined
+loggedInUser = 'Alan Rickman'; // null is now assinged 'alan rickman'
+console.log(`Welcome Back ${loggedInUser}!`); // Alan Rickman
+
+
+// undefined - variables that do not have an assigned value are undefined
+
+let nothingness;
+console.log(nothingness); // undefined 
+// when i declared the variable nothingness i didnt intialize a value
+// so its currently undefined. i havent defined a value for the variable yet
+
+let unLaugh = 'lol';
+console.log(unLaugh[5]); // undefined (there is no character at index 5)
+
+
+
+//Math Object
+//just a collection of math functionality
+//contains properties and methods for mathematical constants and fucntions
+//most the time you use the math object it will be for its methods like
+//rounding or finding absolute value or generating a random number
+
+//syntax
+//                          Math.method()
+// the M is capital ALWAYS
+
+//Math.PI
+//representation of pi
+let pi = Math.PI; 
+console.log(pi); // 3.141592653589793
+
+let pi3 = 3 + Math.PI; // ads the value of 3 to pi
+console.log(pi3); // 6.141592653589793
+
+//Math.round()
+//rounds your number up or down to the nearest whole number
+let floatNumber = 4.5;
+console.log(Math.round(floatNumber)); // 5
+
+let floatNumber2 = 3.2;
+console.log(Math.round(floatNumber2)); // 3
+
+//Math.abs()
+//gives you absolute value of number
+//always returns a positive number
+//good way to turn a negative number positive
+let absNum = -456;
+console.log(Math.abs(absNum)); // 456
+
+//Math.pow()
+//raises to the power of 
+//aka exponent
+//the two values passed in are seperated by a comma
+let powerOf = 6;
+console.log(Math.pow(powerOf, 2)); // 36 (6 to the power of 2)
+
+let powerOfTwo = 2;
+console.log(Math.pow(powerOfTwo, 4)); // 16
+
+//Math.floor()
+//Removes the decimal points from the number
+//floor doesnt round, it just removes all the decimal numbers
+
+let floatNumber3 = 34.560865;
+console.log(Math.floor(floatNumber3)); // 34
+
+let floatNumber4 = 99.99;
+console.log(Math.floor(floatNumber4)); // 99
+
+//Math.random() always gives you a random decimal between 0-1 
+//(non-inclusive) so its necessary to multiply it by 10 to get a whole 
+//number
 //then in order to get rid of the deciamls you just 'floor' it
+
+// start with Math.random() to get a decimal number
 const step1 = Math.random();
 console.log(step1);
 
+//multiply it by 10 to get a whole number with deciaml points
 const step2 = step1 *10;
 console.log(step2);
 
+//Math.floor the previous step to remove the decimal numbers
 const step3 = Math.floor(step2);
 console.log(step3);
 
+//add one to the previous step because indexs start at 0 and we want 1-10 
+//not 0-9
 const step4 = step3 +1;
-console.log(step4);
+console.log(step4); // random number between 1-10
 //the four steps above are the same as the line below
-//the floor gives you the value without the deciaml points
-//then you get a random number with deciamls that floor removes
-//then you multiply it by 10 to get a whole number
-//then you add one because computer does 0-9 rng cuz index
-const randomNumberGenerator = Math.floor(Math.random() * 10) +1;
-console.log(randomNumberGenerator);
 
-//random roll of dice
+const randomNumberGenerator = Math.floor(Math.random() * 10) +1;
+console.log(randomNumberGenerator); // random number between 1-10
 
 let diceRoll = Math.floor(Math.random() *6) +1;
-console.log(diceRoll);
+console.log(diceRoll); // random number between 1-6
+
+
+//typeof !not camelCase! and does not require parthensis with values
+//its an operator like the + or - or / sign.
+//typeof operator is used to determine the type of a given value ie. number,
+//string, boolean etc
+//tells you what type of data something is
+
+let str5 = 'string :)'
+console.log(typeof str5); // string
+
+let floatNumber5 = 4.7;
+console.log(typeof floatNumber5); // number
+
+let boolean2 = true;
+console.log(typeof boolean2); // boolean
+
+//typeof undefined returns undefined
+//typeof null returns object (weird quirk)
+
+
+
+//parseInt & parseFloat
+//extracts a number from a string and turns it into a number as long as the first character of the string is a valid digit
+
+//form data comes as a string. this is one way to extract the numbers to use
+//them mathematically or just to store as a number
+
+// parseInt extracts a whole number from the string its in and makes
+//it a number (of the number type)
+// this comes in handy when a user inputs a number and its receveied as
+// a string
+// mathmatical operators only concatenate strings. so if you need to perform
+// a calculation on a number but its a string, use parseInt to extract the 
+// number from the string
+let num24 = '24'
+console.log(parseInt(num24)); // 24
+
+console.log(parseInt(num24) - 1); // 23
+
+let num25 = '25.975';
+console.log(parseInt(num25)); // 25
+
+let example4 = '28 days later';
+console.log(parseInt(example4)); // 28
+
+//parseFloat does everything parseInt does, except it returns the number 
+//as a float(with all decimal numbers)
+
+let floatNumber6 = '23.456';
+console.log(parseFloat(floatNumber6)); //23.456
+
+//if the number is a whole number it will just display the whole number
+let floatNumber7 = '7';
+console.log(parseFloat(floatNumber7)); // 7
+
+//first character of the string is not a digit so the result is NaN
+let sayingFive = 'i ate 3 shrimp';
+console.log(parseFloat(sayingFive)); // NaN
+
+
 
 
